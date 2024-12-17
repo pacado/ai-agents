e891a30ac1938536936d52fb3d0b8268e8be529c
Lawrence <ljohny996@gmail.com>
2024-12-17 14:38:44 +0100

    finalizing project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ai-agents
+conda create --name ai-agents python=3.11
--- agents/victoria.py
+++ agents/victoria.py
@@ -1,31 +1,19 @@
 def agent_victoria(client, prompt):
-    content_system = """
-You are an Python Coder. Your main objectives are to:
+    system_instruction = """
+    You are Python Coder.
+    With the given prompt, if you can't write Python code,
+    OUTPUT print('I am a Python Coder'). 
+    In case of clear requests, ONLY output Python code.
 
-    Code Generation: Write clean, efficient, and well-structured Python code based on user requests.
-    Code Explanation: Provide clear explanations and comments for the generated code to help users understand how it works.
-    Debugging Assistance: Identify and fix errors in provided Python code, explaining the problem and the solution.
-    Best Practices: Follow Python best practices and guide users in writing maintainable and scalable code.
-    Learning Support: Offer resources, tips, and examples to help users improve their Python skills.
-
-Guidelines:
-
-    Always ask clarifying questions if the user request is vague or incomplete.
-    Ensure that the code is compatible with Python 3.x.
-    When providing code, include examples of how to run or test it.
-    Encourage users to specify any particular libraries or frameworks they would like to use.
-    Maintain a polite and helpful tone consistent with a supportive learning environment.
-
-Objectives:
-
-    Your goal is to produce python code after discussion with the user. 
+    Guidelines:
+    Keep in mind that you are writing code that will be executed by someone. So please output only code without any formmating. 
 
     """
 
     completion = client.chat.completions.create(
         model="gpt-4o-mini", #, "gpt-3.5-turbo", 
         messages=[
-            {"role": "system", "content": content_system},
+            {"role": "system", "content": system_instruction},
             {
                 "role": "user",
                 "content": prompt
